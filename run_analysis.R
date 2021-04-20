530e098af5a6d8bc9863f903285ec8978599eb0a
OursonThermotech <71496222+OursonThermotech@users.noreply.github.com>
2021-04-20 19:40:44 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,25 +4,26 @@ run_analysis <- function (){
 "This script only works if the UCI HAR Dataset folder is unzipped in the working
   directory"
     
-"Q1 : Merging Training and test data"
-  MergedData <- rbind(read.table("./UCI HAR Dataset/train/X_train.txt"),read.table("./UCI HAR Dataset/test/X_test.txt"))
- 
-"Q4 : Appropriately label the data set with descriptive variable names"   
-  NamesTable <- read.table("./UCI HAR Dataset/features.txt")
-  colnames(MergedData) <- NamesTable[,2]
-  
-"Q2 : Selecting Means and Std for each variable"
-  Mean_Std_Data <- select(MergedData, contains("mean")|contains("std"))
-  
-"Q3 : Descriptive activity names"
+"Merging Training and test data for Measurments (X), activities (Y) and subjects"
+  XMergedData <- rbind(read.table("./UCI HAR Dataset/train/X_train.txt"),read.table("./UCI HAR Dataset/test/X_test.txt"))
   YMergedData <- rbind(read.table("./UCI HAR Dataset/train/y_train.txt"),read.table("./UCI HAR Dataset/test/y_test.txt"))
-  ActivityLabel <- (read.table("./UCI HAR Dataset/activity_labels.txt"))
-  Mean_Std_Data <- cbind(Activity = ActivityLabel[YMergedData[,"V1"],"V2"],Mean_Std_Data)
-  
-"Q5 : "  
-  "Subject number attribution"
   SubjectData <- rbind(read.table("./UCI HAR Dataset/train/subject_train.txt"),read.table("./UCI HAR Dataset/test/subject_test.txt"))
-  Mean_Std_Data <- cbind(Subject_Number = SubjectData[,"V1"],Mean_Std_Data)
-  MergedData2 <- Mean_Std_Data%>%group_by(Subject_Number, Activity)%>%summarise_all(mean)
-  return(MergedData2)
+  
+"Attribution of descriptive activity names"
+  ActivityLabel <- (read.table("./UCI HAR Dataset/activity_labels.txt"))
+  YMergedData <- ActivityLabel[YMergedData[,"V1"],"V2"]
+  
+"Merging Subject data, activity data and all variables data"  
+  MergedData <- cbind(SubjectData,YMergedData,XMergedData)
+  
+"Appropriately label the data set with descriptive variable names"   
+  NamesTable <- read.table("./UCI HAR Dataset/features.txt")
+  colnames(MergedData) <- c("Subject_Number","Activity", NamesTable[,2])
+  
+"Selecting Means and Std for each variable and grouping data by subject and activity"
+  MergedData2 <- cbind(MergedData[,1:2], select(MergedData, contains("mean")|contains("std")))
+  MergedData2 <- MergedData2%>%group_by(Subject_Number, Activity)%>%summarise_all(mean)
+  
+"Writing the tidy dataset"    
+  write.table(MergedData2, file="Tidy_Dataset_Submission",row.names = FALSE)
 }
